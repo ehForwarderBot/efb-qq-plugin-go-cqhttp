da15b9bae4ce1a37c0fe9c56e830d6b71be78bf3
milkice233 <milkice@milkice.me>
2021-02-15 23:32:47 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    raise Exception("Python 3.6 or higher is required. Your version is %s." % sys.version)
+
+__version__ = ""
+exec(open('efb_qq_plugin_coolq/__version__.py').read())
+
+long_description = open('README.rst').read()
+
+setup(
+    name='efb-qq-plugin-coolq',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version=__version__,
+    description='EQS plugin for CoolQ API Compatible Client.',
+    long_description=long_description,
+    include_package_data=True,
+    author='Milkice',
+    author_email='milkice@milkice.me',
+    url='https://github.com/milkice233/efb-qq-plugin-coolq',
+    license='GPLv3',
+    python_requires='>=3.6',
+    keywords=['ehforwarderbot', 'EH Forwarder Bot', 'EH Forwarder Bot Slave Channel',
+              'qq', 'chatbot', 'EQS', 'CoolQ'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Communications :: Chat",
+        "Topic :: Utilities"
+    ],
+    install_requires=[
+        "efb-qq-slave", "ehforwarderbot",
+        "PyYaml",
+        'requests', 'python-magic', 'Pillow', 'cqhttp>=1.3.0', 'cherrypy>=18.5.0'
+    ],
+    entry_points={
+        'ehforwarderbot.qq.plugin': 'CoolQ = efb_qq_plugin_coolq:CoolQ'
+    }
+)
